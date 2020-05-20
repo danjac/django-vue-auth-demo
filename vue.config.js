2dc54b6a19b6ded3edfd143037b83e40a12b9fb7
Dan Jacob <danjac2018@gmail.com>
2020-05-20 23:50:44 +0300

    Name changes

--- vue.config.js
+++ vue.config.js
@@ -1,11 +1,4 @@
 module.exports = {
   outputDir: "dist",
   assetsDir: "static",
-  // baseUrl: IS_PRODUCTION
-  // ? 'http://cdn123.com'
-  // : '/',
-  // For Production, replace set baseUrl to CDN
-  // And set the CDN origin to `yourdomain.com/static`
-  // Whitenoise will serve once to CDN which will then cache
-  // and distribute
 };
